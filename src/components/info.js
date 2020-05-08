379054e1d74b5182e69c9f437dc05d12f35d2a49
zaxel <sae11.z.alex@gmail.com>
2020-05-08 15:43:02 +0100

    weather app

---
+++ src/components/info.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+/*full verssion*/
+// class Info extends React.Component{
+//     render(){
+//         return(
+//             <div>
+//                 <h2>Weather App</h2>
+//                 <div>Check the Weather in you town.</div>
+//             </div>
+            
+//         );
+//     }
+// }
+
+
+/*short verssion*/
+const Info =() =>(
+    <div>
+        <h2>Weather App</h2>
+        <div>Check the Weather in you town.</div>
+    </div>
+)
+
+export default Info;
